e05185ea661818b122a521390e261d1aedb177d1
lauxenmaju <161649077+lauxenmaju@users.noreply.github.com>
2024-07-01 22:00:05 -0300

    "firts commit"

---
+++ PROVA_TRIM2_MAJU/att1.py
@@ -0,0 +1,7 @@
+print('MÊS DE NASCIMENTO')
+
+print('Janeiro\n','Fevereiro\n','Março\n','Abril\n','Maio')
+meses=['Janeiro','Fevereiro','Março','Abril','Maio','Junho']
+for mes in meses:
+    if mes =='Junho':
+        print('Nasci em', mes)
---
+++ PROVA_TRIM2_MAJU/att2.js
@@ -0,0 +1,10 @@
+console.log('MES DE NASCIMENTO')
+console.log('Janeiro\n','Fevereiro\n','Março\n','Abril\n','Maio')
+
+let meses=['Janeiro','Fevereiro','Março','Abril','Maio','Junho']
+
+for (let mes of meses){
+    if (mes === 'Junho'){
+        console.log('Nasci em',mes)
+    }
+}
